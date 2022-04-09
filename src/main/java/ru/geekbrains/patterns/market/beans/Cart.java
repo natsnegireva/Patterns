4a272cf36e45bae9a8483b6fe34dc306678af23e
Natalya <natsnegireva@gmail.com>
2022-04-09 21:34:54 +0300

    Patterns homework 3

---
+++ src/main/java/ru/geekbrains/patterns/market/beans/Cart.java
@@ -0,0 +1,53 @@
+package ru.geekbrains.patterns.market.beans;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.geekbrains.patterns.market.exceptions_handling.ResourceNotFoundException;
+import ru.geekbrains.patterns.market.model.OrderItem;
+import ru.geekbrains.patterns.market.model.Product;
+import ru.geekbrains.patterns.market.services.ProductService;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Data
+public class Cart {
+    private final ProductService productService;
+    private List<OrderItem> items;
+    private int totalPrice;
+
+    @PostConstruct
+    public void init() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addToCart(Long id) {
+        for (OrderItem o : items) {
+            if (o.getProduct().getId().equals(id)) {
+                o.incrementQuantity();
+                recalculate();
+                return;
+            }
+        }
+        Product p = productService.findProductById(id).orElseThrow(() -> new ResourceNotFoundException("Unable to find product with id: " + id + " (add to cart)"));
+        OrderItem orderItem = new OrderItem(p);
+        items.add(orderItem);
+        recalculate();
+    }
+
+    public void clear() {
+        items.clear();
+        recalculate();
+    }
+
+    public void recalculate() {
+        totalPrice = 0;
+        for (OrderItem o : items) {
+            totalPrice += o.getPrice();
+        }
+    }
+}
