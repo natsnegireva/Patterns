c7a8c33e2218f0bfbde148323bcac65574df8ee1
Natalya <natsnegireva@gmail.com>
2022-04-10 00:51:18 +0300

    паттерн Factory

--- src/main/java/ru/geekbrains/patterns/market/MarketApplication.java
+++ src/main/java/ru/geekbrains/patterns/market/MarketApplication.java
@@ -6,9 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketApplication {
-	// Корзина является реализацией паттерна Синглтон
-	// Main-класс создает экземпляр ApplicationContext - паттерн Фабрика
+	// ПАТТЕРНЫ В ПРОЕКТЕ:
+
+	// ПОРОЖДАЮЩИЕ ПАТТЕРНЫ:
+	// Корзина является реализацией паттерна Singleton
+	// Main-класс создает экземпляр ApplicationContext - паттерн Factory
 	// реализация заказа использует паттерн Builder
+
+	// СТРУКТУРНЫЕ ПАТТЕРНЫ:
+	// OrederCotroller - паттерн Bridge
+	// OrderService -  паттерн Adapter
+	// Структура проета реализует паттерн Model View Controller
+	// AOP Spring - это паттерн Proxy
+
+	// ПОВЕДЕНЧЕСКИЕ ПАТТЕРНЫ:
+	// AuthController - паттерна Mediator
+	// JwtTokenUtil реализует паттерн Command
+	// Cart реализует паттерн Iterator
+	// В репозиториях List наследуется от Collection extends Iterable - паттерн Iterator
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApplication.class, args);
